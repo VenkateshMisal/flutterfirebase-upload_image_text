d25aadd88097b34b58d0aa5455b49943641287f6
Venkatesh Misal <124044061+VenkateshMisal@users.noreply.github.com>
2023-10-23 16:32:46 +0530

    first commit
    date : 23/10/1999

---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutterfiredemo/add_item.dart';
+import 'package:flutterfiredemo/item_list.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+import 'item_details.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: ItemList(),
+    );
+   
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final CollectionReference _referenceShoppingList =
+      FirebaseFirestore.instance.collection('shopping_list');
+  late Stream<QuerySnapshot> _streamShoppingItems;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+
+    _streamShoppingItems = _referenceShoppingList.snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+   
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await GoogleSignIn().signOut();
+                FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.power_settings_new))
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _streamShoppingItems,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (snapshot.connectionState == ConnectionState.active) {
+            QuerySnapshot querySnapshot = snapshot.data;
+            List<QueryDocumentSnapshot> listQueryDocumentSnapshot =
+                querySnapshot.docs;
+
+            return ListView.builder(
+                itemCount: listQueryDocumentSnapshot.length,
+                itemBuilder: (context, index) {
+                  QueryDocumentSnapshot document =
+                      listQueryDocumentSnapshot[index];
+                  return ShoppingListItem(document: document);
+                });
+          }
+
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => const AddItem()));
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), );
+  }
+}
+
+class ShoppingListItem extends StatefulWidget {
+  const ShoppingListItem({
+    Key? key,
+    required this.document,
+  }) : super(key: key);
+
+  final QueryDocumentSnapshot<Object?> document;
+
+  @override
+  State<ShoppingListItem> createState() => _ShoppingListItemState();
+}
+
+class _ShoppingListItemState extends State<ShoppingListItem> {
+  bool _purchased = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ItemDetails(widget.document.id)));
+      },
+      title: Text(widget.document['name']),
+      subtitle: Text(widget.document['quantity']),
+      trailing: Checkbox(
+        onChanged: (value) {
+          setState(() {
+            _purchased = value ?? false;
+          });
+        },
+        value: _purchased,
+      ),
+    );
+  }
+}
